c0a31f914d1da28814f67d88ec955c71e1ab9ee5
nafees nazik <nafeesnazik10@gmail.com>
2021-12-08 00:34:56 +0530

    add link component

---
+++ src/components/common/Link/Link.tsx
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import NextLink from 'next/link';
+import clsx from 'clsx';
+export interface ILinkProps
+  extends React.DetailedHTMLProps<
+    React.AnchorHTMLAttributes<HTMLAnchorElement>,
+    HTMLAnchorElement
+  > {
+  isExternal?: boolean;
+  href: string;
+  children: React.ReactNode;
+}
+
+export function Link({
+  isExternal,
+  href,
+  children,
+  className,
+  ...rest
+}: ILinkProps) {
+  if (isExternal) {
+    return (
+      <a
+        href={href}
+        className={clsx(
+          'underline hover:text-gray-800 dark:hover:text-gray-200 transition-all',
+          className
+        )}
+        {...rest}
+      >
+        {children}
+      </a>
+    );
+  }
+  return (
+    <NextLink href={href}>
+      <a
+        className={clsx(
+          'underline hover:text-gray-800 dark:hover:text-gray-200 transition-all',
+          className
+        )}
+        {...rest}
+      >
+        {children}
+      </a>
+    </NextLink>
+  );
+}
