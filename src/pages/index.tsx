9b79bb0fb641144e3f0043454b269f0f993f6e09
nafees nazik <nafeesnazik10@gmail.com>
2021-12-08 23:28:58 +0530

    update home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,53 +1,44 @@
-import { Link } from '@/components/common';
+import { IconLink, Link } from '@/components/common';
 import { MainLayout } from '@/components/layout';
 import type { NextPage } from 'next';
-
+import { FiGithub, FiInstagram, FiTwitter, FiMail } from 'react-icons/fi';
 const Home: NextPage = () => {
   return (
     <MainLayout>
-      <div className="flex flex-col justify-center items-start md:h-[80vh] max-w-2xl border-gray-200 dark:border-gray-700 mx-auto pb-16">
-        <div className="flex flex-col-reverse sm:flex-row items-start">
-          <div className="flex flex-col pr-8">
-            <h1 className="font-bold text-3xl md:text-5xl  mb-1 text-black dark:text-white font-cal">
-              Nafees Nazik
-            </h1>
-            <h2 className="text-gray-700 dark:text-gray-200 mb-4">
-              A self taught developer
-            </h2>
-            <p className="text-gray-600 dark:text-gray-400 mb-4">
-              Hey, I am Nafees Nazik, a software developer from sri lanka. i
-              work Mostly with React and javascript ecosystem, which are open
-              source and publicly available on{' '}
-              <Link href="https://github.com/G3root" isExternal>
-                GitHub
-              </Link>
-              .
-            </p>
-            <p className="text-gray-600 dark:text-gray-400 mb-16">
-              You can find me on{' '}
-              <Link href="https://github.com/G3root" isExternal>
-                GitHub
-              </Link>
-              ,{' '}
-              <Link href="https://twitter.com/nfs__21" isExternal>
-                Twitter
-              </Link>
-              ,{' '}
-              <Link
-                href="https://www.instagram.com/nafees_nazik.21/"
-                isExternal
-              >
-                Instagram
-              </Link>{' '}
-              or mail me at{' '}
-              <Link href="mailto:nafeesnazik21@gmail.com" isExternal>
-                nafeesnazik21@gmail.com
-              </Link>
-              .
-            </p>
+      <section id="hero" className="pt-8 md:pt-32">
+        <div className="max-w-7xl mx-auto ">
+          <div className="font-black grid grid-cols-12 gap-5">
+            <div className="col-start-1 col-end-12 lg:col-end-10">
+              <h1 className="text-5xl md:text-7xl tracking-tighter font-bold md:mb-3 text-gray-800 dark:text-white">
+                Developer who loves creating interfaces and websites.
+              </h1>
+            </div>
+          </div>
+          <p className=" text-gray-600 dark:text-gray-400 max-w-3xl  mt-10">
+            Hey, I’m Nafees Nazik, a Sri Lanka based software developer. I
+            specialise in React and javascript ecosystem, which are mainly open
+            source and publicly available on my{' '}
+            <Link href="https://github.com/G3root" isExternal>
+              GitHub
+            </Link>
+            .
+          </p>
+          <div className="flex items-center mt-5 space-x-4">
+            <IconLink href="https://github.com/G3root">
+              <FiGithub className="h-5 w-5" />
+            </IconLink>
+            <IconLink href="https://www.instagram.com/nafees_nazik.21/">
+              <FiInstagram className="h-5 w-5" />
+            </IconLink>
+            <IconLink href="https://twitter.com/nfs__21">
+              <FiTwitter className="h-5 w-5" />
+            </IconLink>
+            <IconLink href="mailto:nafeesnazik21@gmail.com">
+              <FiMail className="h-5 w-5" />
+            </IconLink>
           </div>
         </div>
-      </div>
+      </section>
     </MainLayout>
   );
 };
