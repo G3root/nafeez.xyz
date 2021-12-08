34d6f42b6d04aaee5843e8d1ab91e981dbc979be
nafees nazik <nafeesnazik10@gmail.com>
2021-12-08 14:52:37 +0530

    update styles in navbar component

--- src/components/common/Navbar/Navbar.tsx
+++ src/components/common/Navbar/Navbar.tsx
@@ -8,7 +8,7 @@ export interface INavbarProps {}
 export function Navbar(props: INavbarProps) {
   return (
     <div className="flex flex-col justify-center px-8">
-      <nav className="flex items-center justify-between w-full relative  border-gray-200 dark:border-gray-700 mx-auto pt-8 pb-8 sm:pb-16  text-gray-900 bg-gray-50  dark:bg-gray-900 bg-opacity-60 dark:text-gray-100">
+      <nav className="flex max-w-7xl items-center justify-between w-full relative  border-gray-200 dark:border-gray-700 mx-auto pt-8 pb-8 sm:pb-16  text-gray-900 bg-gray-50  dark:bg-gray-900 bg-opacity-60 dark:text-gray-100">
         <a href="#skip" className="skip-nav">
           Skip to content
         </a>
