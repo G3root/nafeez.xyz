f60570ed15c9815dc56a9b0bafe82c28ac3ca043
nafees nazik <nafeesnazik10@gmail.com>
2021-12-13 23:47:38 +0530

    add tailwindcss forms plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,5 +29,5 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [require('@tailwindcss/line-clamp'),],
+  plugins: [require('@tailwindcss/line-clamp'), require('@tailwindcss/forms'),],
 }
