d511fe33bc718ac096aafaf0d7519a46fd7f56bd
nafees nazik <nafeesnazik10@gmail.com>
2021-12-13 15:56:32 +0530

    type opeansea asset

---
+++ src/types/opensea.ts
@@ -0,0 +1,10 @@
+export interface OpenSeaAsset {
+  id: number;
+  name: string;
+  image_url: string;
+  image_preview_url: string;
+  image_original_url: string;
+  image_thumbnail_url: string;
+  permalink: string;
+  description: string;
+}
