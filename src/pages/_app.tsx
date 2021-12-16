c0478deedd95656d8ce674b9a36128c092356188
nafees nazik <nafeesnazik10@gmail.com>
2021-12-16 15:03:45 +0530

    add react hot toast

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,14 @@ import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
 import { KbarModal } from '@/components/common';
 import { SessionProvider } from 'next-auth/react';
+import { Toaster } from 'react-hot-toast';
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
       <ThemeProvider attribute="class">
         <KbarModal>
           <Component {...pageProps} />
+          <Toaster />
         </KbarModal>
       </ThemeProvider>
     </SessionProvider>
