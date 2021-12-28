5543e0adf27e7bfb2c0b1e42b48ac7993278ca2d
nafees nazik <nafeesnazik10@gmail.com>
2021-12-28 12:25:55 +0530

    add poap to gallery

--- src/pages/nft-gallery.tsx
+++ src/pages/nft-gallery.tsx
@@ -1,6 +1,7 @@
 import { MainLayout } from '@/components/layout';
 import type { InferGetStaticPropsType } from 'next';
 import type { OpenSeaAsset } from '@/types/opensea';
+import type { PoapAsset } from '@/types/poap';
 import { NftCard } from '@/components/nft-gallery';
 import { Link } from '@/components/common';
 type NftMetadata = {
@@ -11,15 +12,19 @@ export const getStaticProps = async () => {
   const req = await fetch(
     'https://api.opensea.io/api/v1/assets?owner=0xD2eCE15856813709Dd181A55c9fC82059Fdb2E2c&order_direction=desc&offset=0&limit=20'
   );
+  const poapReq = await fetch(
+    'https://api.poap.xyz/actions/scan/0xD2eCE15856813709Dd181A55c9fC82059Fdb2E2c'
+  );
   const res: NftMetadata = await req.json();
-
+  const poapRes: PoapAsset[] = await poapReq.json();
   return {
-    props: { ...res, revalidate: 60 * 60 * 4 } //revalidate every 4 hours
+    props: { ...res, poap: poapRes, revalidate: 60 * 60 * 4 } //revalidate every 4 hours
   };
 };
 
 const NftGallery = ({
-  assets
+  assets,
+  poap
 }: InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
     <MainLayout>
@@ -29,20 +34,10 @@ const NftGallery = ({
         </h1>
         <div>
           <p className="mb-4 text-gray-600 dark:text-gray-400 max-w-lg">
-            These are the collection of nft&apos;s I own and also check out my
-            account on{' '}
-            <Link
-              isExternal
-              href="https://opensea.io/nfs__21"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              OpenSea
-            </Link>
-            .
+            These are the collection of nft&apos;s and poap&apos;s I own .
           </p>
         </div>
-        <div className="grid w-full grid-cols-1 gap-4 my-2 mt-4 sm:grid-cols-4">
+        <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8 ">
           {assets && assets.length > 0
             ? assets.map(({ name, description, id, image_url, permalink }) => (
                 <NftCard
@@ -51,6 +46,22 @@ const NftGallery = ({
                   permalink={permalink}
                   imageUrl={image_url}
                   key={id}
+                />
+              ))
+            : null}
+        </div>
+        <h2 className="text-3xl mt-10  font-extrabold tracking-tight ">
+          Collected POAP&apos;s
+        </h2>
+        <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8 ">
+          {poap && poap.length > 0
+            ? poap.map(({ event, tokenId }) => (
+                <NftCard
+                  name={event.name}
+                  description={event.description}
+                  permalink={`https://app.poap.xyz/token/${tokenId}`}
+                  imageUrl={event.image_url}
+                  key={tokenId}
                 />
               ))
             : null}
