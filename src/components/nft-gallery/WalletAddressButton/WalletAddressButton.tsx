e0c78d923ece1afcff350a5cb71d918ab0366342
nafees nazik <nafeesnazik10@gmail.com>
2022-01-03 13:44:23 +0530

    add wallet address button

---
+++ src/components/nft-gallery/WalletAddressButton/WalletAddressButton.tsx
@@ -0,0 +1,26 @@
+import { useClipboard } from '@/hooks/utils';
+import * as React from 'react';
+import { FiCopy, FiCheck } from 'react-icons/fi';
+export interface IWalletAddressButtonProps {}
+const walletAddress = '0xD2eCE15856813709Dd181A55c9fC82059Fdb2E2c';
+
+export function WalletAddressButton(props: IWalletAddressButtonProps) {
+  const { copy, copied } = useClipboard({ timeout: 1000 });
+  return (
+    <button
+      onClick={() => {
+        copy(walletAddress);
+      }}
+      className="inline-flex items-center py-px px-2 bg-gray-800 rounded-md shadow-sm text-sm"
+      aria-label="copy wallet address to clipboard"
+      title={walletAddress}
+    >
+      0xD2e...2E2c{' '}
+      {copied ? (
+        <FiCheck className="ml-2" aria-hidden />
+      ) : (
+        <FiCopy className="ml-2" aria-hidden />
+      )}
+    </button>
+  );
+}
--- src/components/nft-gallery/WalletAddressButton/index.ts
+++ src/components/nft-gallery/WalletAddressButton/index.ts
@@ -1,2 +1,1 @@
-export * from './NftCard';
 export * from './WalletAddressButton';
