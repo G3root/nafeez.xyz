266ff05191f95a87f0db390c4a9edd4ac78552e3
nafees nazik <nafeesnazik10@gmail.com>
2022-01-03 13:48:20 +0530

    remove console logs

--- src/pages/api/guestbook/delete/[id].ts
+++ src/pages/api/guestbook/delete/[id].ts
@@ -57,7 +57,6 @@ export default async function handler(
           id: result.data.id
         }
       });
-      console.log(deleteEntry);
 
       return res.status(200).json({ success: true });
     } catch (error) {
